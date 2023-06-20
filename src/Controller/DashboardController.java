d386185a56ca10fae445f699a6d3f1203e39ead3
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-06-20 23:23:00 +0700

    update

---
+++ src/Controller/DashboardController.java
@@ -0,0 +1,61 @@
+package Controller;
+
+import Model.SongDTO;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    @FXML
+    private HBox favoriteContainer;
+
+    @FXML
+    private HBox recentlyPlayedContainer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        dsRecentlyPlayed = new ArrayList<>(getRecentlyPlayed());
+        try {
+            for (SongDTO song : dsRecentlyPlayed) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/View/song.fxml"));
+
+                VBox vbox = fxmlLoader.load();
+                SongController songController = fxmlLoader.getController();
+                songController.setData(song);
+
+                recentlyPlayedContainer.getChildren().add(vbox);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ArrayList<SongDTO> dsRecentlyPlayed;
+    ArrayList<SongDTO> dsFavorite;
+
+
+    private ArrayList<SongDTO> getRecentlyPlayed() {
+        ArrayList<SongDTO> listRP = new ArrayList<>();
+        SongDTO song = new SongDTO();
+        song.setName("In The Name Of Love");
+        song.setArtist("Martin Garrix, Bebe Rexha");
+        song.setCover("/img/In_the_Name_of_Love.jpeg");
+
+        listRP.add(song);
+
+        song = new SongDTO();
+        song.setName("Emri Olur");
+        song.setArtist("Mustafa Cecili");
+        song.setCover("/img/emri_olur.jpg");
+
+        return listRP;
+    }
+}
--- src/Controller/SongController.java
+++ src/Controller/SongController.java
@@ -1,4 +1,27 @@
 package Controller;
 
+import Model.SongDTO;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+
 public class SongController {
+    @FXML
+    private Label artist;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label songName;
+
+    public void setData(SongDTO song){
+        Image image = new Image(getClass().getResourceAsStream(song.getCover()));
+        img.setImage(image);
+        artist.setText(song.getArtist());
+        songName.setText(song.getName());
+        
+    }
 }
